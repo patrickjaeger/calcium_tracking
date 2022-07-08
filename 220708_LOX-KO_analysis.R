53287620ca6c4cce40f11519f5c67dec79980629
Patrick <patrick.jaeger@hest.ethz.ch>
2022-07-08 15:52:11 +0200

    Add cumulative activity function

--- 220708_LOX-KO_analysis.R
+++ 220708_LOX-KO_analysis.R
@@ -26,3 +26,44 @@ classify_signals <- function(.df, .thresh = 1.1) {
 
 dat <- classify_signals(dat1245)
 rm(dat1245)
+
+
+
+
+
+
+cum_activity <- function(.dat) {
+  # Add the cumulative cell activity to the data
+  .dat %>% 
+    mutate(n_cells = map_int(data, ~n_distinct(.$cell_id))) %>% 
+    unnest(data) %>% 
+    filter(first_peak != Inf) %>%
+    group_by(dataset, condition, img_id, cell_id, n_cells) %>% 
+    slice(1) %>% 
+    # ungroup() %>% 
+    group_by(dataset, condition, img_id, first_peak, n_cells) %>% 
+    summarise(n_active = n()) %>% 
+    group_by(dataset, condition, img_id) %>% 
+    mutate(cum_activity = cumsum(n_active),
+           norm_cum_activity = cum_activity/max(cum_activity),
+           n_cells_norm_cum_activity = cum_activity/max(n_cells))
+}
+
+datc <- cum_activity(dat)
+
+ggplot(datc, aes(first_peak, 
+                 n_cells_norm_cum_activity, 
+                 group = interaction(dataset, img_id), 
+                 color = condition)) +
+  geom_line() +
+  geom_vline(xintercept = c(10, 210), lty = 3) +
+  # scale_color_manual(values = c('black', 'skyblue')) +
+  labs(x = 'Time [s]', y = 'Active cells [%]', color = '',
+       title = 'Cumulative calcium signals at 0.5%/s to 50% max. strain') +
+  theme_bw() + 
+  theme(legend.position = 'top')
+
+
+find_threshold <- function(.dat) {
+  
+}
