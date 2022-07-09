6fc026c4d52153d8071113b067ef2c0ba982d7b9
Patrick <patrick.jaeger@hest.ethz.ch>
2022-07-09 11:18:54 +0200

    Improve classify_signals() description

--- 220708_LOX-KO_analysis.R
+++ 220708_LOX-KO_analysis.R
@@ -10,15 +10,23 @@ load("data/220707_LOX_KO_1245.RData")
 ## How to set the activation threshold?
 
 classify_signals <- function(.df, .thresh = 1.1) {
-  # Extract the time when a cell first surpasses the threshold
+  # Classify the cells into active/inactive based on whether their signal
+  # intensity surpasses the threshold set
+  
+  # Renest into single cells
   .df %>% 
     unnest(data) %>% 
     group_nest(across(1:11)) %>% 
+    
+  # Find the first datapoint that surpasses the threshold
     mutate(first_peak = map_dbl(data, ~min(which(.$calcium > .thresh)))) %>%
-    suppressWarnings() %>%  # Every Inf value gives a warning
-    # If the threshold is never reached (Inf), the cell is not active (0)
-    # else it's active (1)
+    suppressWarnings() %>% 
+    
+  # If the threshold is never reached (Inf), the cell is not active (0)
+  # else it's active (1)
     mutate(peak_type = if_else(first_peak == Inf, 0, 1)) %>%
+    
+  # Renest data into samples
     unnest(data) %>% 
     group_nest(across(1:10))
 }
@@ -33,7 +41,7 @@ rm(dat1245)
 
 
 cum_activity <- function(.dat) {
-  # Add the cumulative cell activity to the data
+  # Calculate the cumulative cell activity and count the cells
   .dat %>% 
     mutate(n_cells = map_int(data, ~n_distinct(.$cell_id))) %>% 
     unnest(data) %>% 
@@ -51,13 +59,13 @@ cum_activity <- function(.dat) {
 
 datc <- cum_activity(dat)
 
+# TODO The time scale is messed up: it should only go to 155, not 620
 ggplot(datc, aes(first_peak, 
                  n_cells_norm_cum_activity, 
                  group = interaction(dataset, img_id), 
                  color = condition)) +
   geom_line() +
   geom_vline(xintercept = c(10, 210), lty = 3) +
-  # scale_color_manual(values = c('black', 'skyblue')) +
   labs(x = 'Time [s]', y = 'Active cells [%]', color = '',
        title = 'Cumulative calcium signals at 0.5%/s to 50% max. strain') +
   theme_bw() + 
